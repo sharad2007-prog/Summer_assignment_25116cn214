cb2a18b583c97697ea5803c862e4ea171034fd19
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:09:18 +0530

    Update Q43.c

---
+++ Day 11/Q43.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Function to check prime number
+int isPrime(int n)
+{
+    if(n <= 1)
+        return 0;
+
+    for(int i = 2; i <= n / 2; i++)
+    {
+        if(n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if(isPrime(n))
+        printf("Prime Number");
+    else
+        printf("Not a Prime Number");
+
+    return 0;
+}
