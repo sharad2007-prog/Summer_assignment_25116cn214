dc0a0a1e0a7a0eb73006597c963e786bda0582d9
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 13:56:23 +0530

    Update Q38.c

---
+++ Day 10/Q38.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    // Print reverse pyramid
+    for(int i = rows; i >= 1; i--)
+    {
+        for(int j = 1; j <= rows - i; j++)
+            printf(" ");
+
+        for(int j = 1; j <= (2 * i - 1); j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
