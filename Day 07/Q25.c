5be9cb1c19948dfe4a1f0e55d7a3c24d16c9a7bc
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:03:36 +0530

    Update Q25.c

---
+++ Day 07/Q25.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Recursive function for factorial
+long long factorial(int n)
+{
+    if(n == 0 || n == 1)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %lld", factorial(n));
+
+    return 0;
+}
