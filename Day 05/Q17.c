517e38366ead77bb5d13bbe129b87016a05a7067
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:02:10 +0530

    Update Q17.c

---
+++ Day 05/Q17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Find sum of factors
+    for(int i = 1; i < n; i++)
+    {
+        if(n % i == 0)
+            sum += i;
+    }
+
+    if(sum == n)
+        printf("Perfect Number");
+    else
+        printf("Not a Perfect Number");
+
+    return 0;
+}
