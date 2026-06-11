5b0ed91d52989352059a5d566737c1a6310657ae
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:05:58 +0530

    Update Q34.c

---
+++ Day 09/Q34.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    // Print reverse number triangle
+    for(int i = rows; i >= 1; i--)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
