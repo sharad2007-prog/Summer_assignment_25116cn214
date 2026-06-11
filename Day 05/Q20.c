39f0d3f25f782cc837fa08abd96208fb9eb82678
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:02:55 +0530

    Update Q20.c

---
+++ Day 05/Q20.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n, largest = -1;
+
+    printf("Enter a number: ");
+    scanf("%lld", &n);
+
+    // Remove factor 2
+    while(n % 2 == 0)
+    {
+        largest = 2;
+        n /= 2;
+    }
+
+    // Check odd factors
+    for(long long i = 3; i * i <= n; i += 2)
+    {
+        while(n % i == 0)
+        {
+            largest = i;
+            n /= i;
+        }
+    }
+
+    if(n > 2)
+        largest = n;
+
+    printf("Largest Prime Factor = %lld", largest);
+
+    return 0;
+}
