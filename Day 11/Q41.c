290d50756211a3de6ab6ba6a52677f6df80f7656
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:08:54 +0530

    Update Q41.c

---
+++ Day 11/Q41.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Function to calculate sum
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &x, &y);
+
+    printf("Sum = %d", sum(x, y));
+
+    return 0;
+}
