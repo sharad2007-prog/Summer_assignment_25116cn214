bbfcf0b29aaf752187855b6831cc6e4bd25b57c7
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-11 14:02:41 +0530

    Update Q19.C

---
+++ Day 05/Q19.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factors are: ");
+
+    // Print all factors
+    for(int i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
