3d0ac96c86c8f8f10ad8697ec6fa7246f800dc2d
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-18 21:46:50 +0530

    Create Q69.py

---
+++ Day 18/Q69.py
@@ -0,0 +1,9 @@
+#bubble sort 
+arr = [5, 3, 8, 4, 2]
+
+for i in range(len(arr)):
+    for j in range(len(arr) - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:", arr)
