cf9bf67eee645d5e10a2e0d0a5d585db9602348b
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-22 15:08:55 +0530

    Create Q86.py

---
+++ Day 22/Q86.py
@@ -0,0 +1,10 @@
+# Input sentence from user
+sentence = input("Enter a sentence: ")
+
+# Split sentence into words
+words = sentence.split()
+
+# Count number of words
+count = len(words)
+
+print("Number of words:", count)
