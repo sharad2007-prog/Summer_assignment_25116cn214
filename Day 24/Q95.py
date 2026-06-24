c627822b5ad354ea42359bc994ddd2a1e204b218
Sharad Ojha <ojhasharad20@gmail.com>
2026-06-24 22:31:07 +0530

    Add script to find the longest word in a sentence

---
+++ Day 24/Q95.py
@@ -0,0 +1,11 @@
+x = input("Enter a sentence: ")
+
+words = x.split()
+
+longest = words[0]
+
+for word in words:
+    if len(word) > len(longest):
+        longest = word
+
+print("Longest word is:", longest)
